d0010f18abe37e44e6870e78fb10565a8c37ca6c
RestiveSole267 <32211667+RestiveSole267@users.noreply.github.com>
2019-03-07 08:56:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Commando = require ('discord.js-commando');
 const bot = new Commando.Client();
 
 
-
-
+bot.registry.registerGroup('simple','Simple');
+bot.registry.registerDefaults();
+bot.registry.registerCommandIn(__dirname + "/commands");
 
 bot.login('');
